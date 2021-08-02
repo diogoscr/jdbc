2df573f0a43d69653a2466c10afed5e902bd2378
diogoscr <diogo.arauj@gmail.com>
2021-08-02 16:28:40 -0300

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,15 +1,17 @@
 package application;
 
-import db.DB;
 import model.entities.Department;
-
-import java.sql.Connection;
+import model.entities.Seller;
+import java.util.Date;
 
 public class Program {
 
     public static void main(String[] args) {
 
         Department obj = new Department(1,"Books");
-        System.out.println(obj);
+
+        Seller seller = new Seller(21,"Bob Geldof","bobgel@gmail.com", new Date(),3000.0,obj);
+
+        System.out.println(seller);
     }
 }
